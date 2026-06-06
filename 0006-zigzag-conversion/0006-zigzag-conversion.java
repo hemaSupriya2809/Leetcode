eda1f849744295d35460ab11283386cf4b3ff530
hemaSupriya2809 <hemasupriyat@gmail.com>
2026-06-06 22:13:06 +0530

    Time: 7 ms (21.45%), Space: 47.1 MB (22.54%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String convert(String s, int numRows) {
+       if(numRows==1){
+        return s;
+       } 
+       StringBuilder rows[]=new StringBuilder[numRows];
+       for(int i=0;i<numRows;i++){
+        rows[i]=new StringBuilder();
+       }
+       int row=0;
+       boolean d=true;
+       for(int i=0;i<s.length();i++){
+         rows[row].append(s.charAt(i));
+         if(row==numRows-1){
+            d=false;
+         }
+         else if(row==0){
+            d=true;
+         }
+         if(d){
+            row++;
+         }
+         else{
+            row--;
+         }
+       }
+       String a="";
+       for(int i=0;i<numRows;i++){
+        a+=rows[i].toString();
+       }
+       return a;
+
+    }
+}

c0fa9451626783d7be3f06466f061a4a39c4b029
hemaSupriya2809 <hemasupriyat@gmail.com>
2026-08-21 09:40:14 +0530

    Time: 0 ms (100%), Space: 41.7 MB (95.24%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int nextGreaterElement(int n) {
+        char[] a = String.valueOf(n).toCharArray();
+        int i = a.length - 2;
+        while (i >= 0 && a[i] >= a[i + 1]) {
+            i--;
+        }
+        if (i < 0) {
+            return -1;
+        }
+        int j = a.length - 1;
+        while (a[j] <= a[i]) {
+            j--;
+        }
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        int left = i + 1;
+        int right = a.length - 1;
+        while (left < right) {
+            temp = a[left];
+            a[left] = a[right];
+            a[right] = temp;
+            left++;
+            right--;
+        }
+        long ans = Long.parseLong(new String(a));
+        if (ans > Integer.MAX_VALUE) {
+            return -1;
+        }
+        return (int) ans;
+    }
+}

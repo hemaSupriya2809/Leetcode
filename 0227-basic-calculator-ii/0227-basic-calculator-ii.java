79dd37c12b75c63fc041b942744960b2e8b70f1f
hemaSupriya2809 <hemasupriyat@gmail.com>
2026-08-28 14:22:27 +0530

    Time: 13 ms (92.82%), Space: 48.6 MB (22.25%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void evaluateOpr(char prevOpr, int num, Deque<Integer> nums) {
+        if (prevOpr == '/')
+            nums.addLast(nums.removeLast() / num);
+        else if (prevOpr == '*')
+            nums.addLast(nums.removeLast() * num);
+        else if (prevOpr == '+')
+            nums.addLast(num);
+        else
+            nums.addLast(num * -1);
+    }
+    
+    
+    public int calculate(String s) {
+        int num = 0;
+        char prevOpr = '+';
+        Deque<Integer> nums = new ArrayDeque<>();
+        
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == ' ')
+                continue;
+            
+            
+            if (Character.isDigit(ch)) {
+                num = num * 10 + ch - '0';
+            } else {
+                evaluateOpr(prevOpr, num, nums);
+                prevOpr = ch;
+                num = 0;
+            }
+        }
+        
+        
+        evaluateOpr(prevOpr, num, nums);
+        num = 0;
+        
+        
+        for (int n : nums)
+            num += n;
+        
+        
+        return num;
+    }
+}
